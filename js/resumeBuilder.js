a8dbd25dc9d779d1b6ebf518759f09d45e4faaa4
Juan Manuel Pacheco <paxe87@gmail.com>
2016-06-23 23:42:44 +0200

    Lesson 2(WIP)

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,28 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-$("#main").append("jmpacheco");
+// $("#main").append("jmpacheco");
+
+var email = "myemail@gmail.com";
+var newemail = email.replace("myemail" , "newemail");
+
+console.log(email);
+console.log(newemail);
+
+var awesomeThoughts = "I am someone and I am awesome";
+var funThoughts = awesomeThoughts.replace("awesome", "fun");
+
+//$("#main").append(funThoughts);
+var name = "someone";
+var role = "software engineer";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+
+var astring = "audacity";
+var astringconverted = astring[1].toUpperCase() + astring.slice(2);
+console.log(astring + " -> " + astringconverted);
+
