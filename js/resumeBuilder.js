4a22dd8ca8c246a154cc373e6a7aa154a28aa5f8
Juan Manuel Pacheco <paxe87@gmail.com>
2016-06-24 17:27:20 +0200

    Quiz: Work! solutions

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -17,9 +17,18 @@ var bio = {
 var work = {
     "jobs" : [  
     {
-        "position" : "Engineer",
-        "years" : "40",
-        "city" : "Madrid"
+        "employer" : "World Company",
+        "title" : "Engineer",
+        "years" : "2013-2014",
+        "city" : "Madrid",
+        "description": "Development"
+    },
+    {
+        "employer" : "Universe Company",
+        "title" : "Engineer",
+        "years" : "2015-2016",
+        "city" : "Chicago",
+        "description" : "Management"
     }
     ]
 };
@@ -43,7 +52,7 @@ var education = {
   {
      "title": "JavaScript",
      "school": "Udacity",
-     "dates": 2016,
+     "dates": "2016"
   }
 
 ]
@@ -59,13 +68,43 @@ var projects = {
     ]
 };
 
-$("#main").append(bio.age);
-$("#main").append(bio.skills.join(" "));
-$("#main").append(bio.picture);
-$("#main").append(bio.welcomeMessage);
+//$("#main").append(bio.age);
+//$("#main").append(bio.skills.join(" "));
+//$("#main").append(bio.picture);
+//$("#main").append(bio.welcomeMessage);
 
-$("#main").append(work.position);
-$("#main").append(education.name);
+//$("#main").append(work.position);
+//$("#main").append(education.name);
+
+console.log(bio.name + " " + bio.role);
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+if (bio.skills.length > 0){
+    console.log("There are skills:" + bio.skills.join(" "));
+    $("#header").append(HTMLskillsStart);
+    for (skillIdx in bio.skills){
+        $("#skills").append(HTMLskills.replace("%data%", bio.skills[skillIdx]));
+    }
+}
+
+for (job in work.jobs) {
+    $("#workExperience").append(HTMLworkStart);
+    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
 
 
+    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+    var formattedEmployerTitle = formattedEmployer + " " + formattedTitle;
+    $(".work-entry:last").append(formattedEmployerTitle);
+
+    var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].years);
+    $(".work-entry:last").append(formattedDates);
+
+    var formattedDesc = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+    $(".work-entry:last").append(formattedDesc);
+
+}
 
