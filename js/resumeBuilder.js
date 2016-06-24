122154f6bd4521abf267aee86e920ad56faab918
jmpacheco <jmpacheco@zhilabs.com>
2016-06-24 07:40:47 -0300

    Lesson Data Types Changes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,28 +1,71 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-// $("#main").append("jmpacheco");
+var bio = {
+    "name" : "someone",
+    "age" : "20",
+    "role" : "software engineer",
+    "skills": ["hardworker", "awesomeness", "Engineer"],
+    "contact" : {
+        "mobile": "666-666-666",
+        "email": "someone@gmail.com"
+    },
+    "bioPic" : "images/goku.jpg",
+    "welcomeMessage" : "lorem ipsum dolor sit amet etc etc etc."
+};
 
-var email = "myemail@gmail.com";
-var newemail = email.replace("myemail" , "newemail");
+var work = {
+    "jobs" : [  
+    {
+        "position" : "Engineer",
+        "years" : "40",
+        "city" : "Madrid"
+    }
+    ]
+};
 
-console.log(email);
-console.log(newemail);
+var education = {
+    "schools": [
+    {
+     "name" : "Street School",
+     "degree" : "Engineer",
+     "years" : "10",
+     "city" : "Madrid"
+    },
+    {
+     "name" : "City School",
+     "years" : "10",
+     "degree" : "Basic Studies",
+     "city" : "Madrid"
+    }
+  ],
+  "onlineCourser": [
+  {
+     "title": "JavaScript",
+     "school": "Udacity",
+     "dates": 2016,
+  }
 
-var awesomeThoughts = "I am someone and I am awesome";
-var funThoughts = awesomeThoughts.replace("awesome", "fun");
+]
+}
 
-//$("#main").append(funThoughts);
-var name = "someone";
-var role = "software engineer";
+var projects = {
+    "projects": [
+    {
+        "title": "Builder",
+        "dates":"2014",
+        "description": "Build houses"
+    }
+    ]
+};
 
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+$("#main").append(bio.age);
+$("#main").append(bio.skills.join(" "));
+$("#main").append(bio.picture);
+$("#main").append(bio.welcomeMessage);
+
+$("#main").append(work.position);
+$("#main").append(education.name);
 
 
-var astring = "audacity";
-var astringconverted = astring[1].toUpperCase() + astring.slice(2);
-console.log(astring + " -> " + astringconverted);
 
